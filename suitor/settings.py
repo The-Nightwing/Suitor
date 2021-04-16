740c0d821af5004681359706360473479422cf6f
Shivam <vermashivam0606@gmail.com>
2021-04-16 21:45:14 +0530

    Read for deploy

--- suitor/settings.py
+++ suitor/settings.py
@@ -17,6 +17,7 @@ import django_heroku
 from decouple import config
 pymysql.install_as_MySQLdb()
 
+os.getenv(".env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
